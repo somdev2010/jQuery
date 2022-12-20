f96917c90f347d38282bace9818dc440904724c7
Somdev Mitra <somdev_55@outlook.com>
2022-12-20 14:57:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function func1(){
+$("#img1").fadeToggle(1000);
+}
+function func2(){
+    //$(element name)
+    // document.getElementsByTagName(element Name)
+    // $("div").css('background-color','orange')
+    // $("#p1").css('font-style','italic')
+    // $(".para").css('font-style','italic')
+    // $("#div1,#div2").css('background-color','green')
+    // $("#div1, li").css('background-color','purple')
+    // $("div  p").fadeToggle();
+    // $("p:first").fadeToggle();
+    $("li:odd").fadeToggle();
+}
+
---
+++ script2.js
@@ -0,0 +1,15 @@
+// function func1(){
+//     document.getElementById("img1").style.width="500px"
+// }
+// $("button").click(func1);
+// function func1(){
+//     $("#img1").css('width','500px')
+// } 
+    // $("btn").click(func1);
+    // function func1(){
+    //     $("#img1").css('width','500px')}
+    $("#img1").mouseleave(function(){
+    $("#img1").css('width','1600px');})
+
+
+
